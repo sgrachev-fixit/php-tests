1f6f294d1269107c32807d99017d602a0e099e64
sgrachev-fixit <sgrachev.fixit@gmail.com>
2022-10-09 20:08:50 +0300

    js test improved

--- path.js
+++ path.js
@@ -1,16 +1,5 @@
 function find(object, path) {
-
-    path_parts = path.split('.');
-    parts_count = path_parts.length;
-    working_object = object  
   
-    for (var i=0; i<parts_count; i++){
-        if (working_object.hasOwnProperty(path_parts[i])) {
-              working_object = working_object[path_parts[i]];    
-            } else {
-              return undefined;
-            }          
-    };
+  return  path.split('.').reduce ( (obj, prop) => (typeof obj != "undefined") ? ( obj.hasOwnProperty(prop) ? obj[prop] : undefined) : undefined , object );
   
-    return working_object;  
 }
